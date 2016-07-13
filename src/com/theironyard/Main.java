7edfbd536ba497a12dbbdbf4d1776969938cede1
willriggins <williamriggins2013@gmail.com>
2016-07-13 11:25:15 -0400

    functional maze!

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,110 @@
 package com.theironyard;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Main {
 
+    static final int SIZE = 10;
+
+    static Room[][] createRooms() {
+        Room[][] rooms = new Room[SIZE][SIZE];
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                rooms[row][col] = new Room(row, col);
+            }
+        }
+        return rooms;
+    }
+
+    static ArrayList<Room> possibleNeighbors(Room[][] rooms, int row, int col) {
+        ArrayList<Room> neighbors = new ArrayList<>();
+
+        // get top room:
+        if (row > 0) {
+            neighbors.add(rooms[row - 1][col]);
+        }
+
+        // get bottom room:
+        if (row < SIZE - 1) {
+            neighbors.add(rooms[row + 1][col]);
+        }
+
+        //get left room:
+        if (col > 0) {
+            neighbors.add(rooms[row][col - 1]);
+        }
+
+        //get right room:
+        if (col < SIZE - 1) {
+            neighbors.add(rooms[row][col + 1]);
+        }
+
+        neighbors = neighbors.stream()
+                .filter(room -> !room.wasVisited)
+                .collect(Collectors.toCollection(ArrayList<Room>::new)); // end is to collect it back into an arraylist of rooms
+        return neighbors;
+    }
+
+    static Room randomNeighbor(Room[][] rooms, int row, int col) {
+        ArrayList<Room> neighbors = possibleNeighbors(rooms, row, col);
+
+        if (neighbors.size() > 0) {
+            Random r = new Random();
+            int index = r.nextInt(neighbors.size()); // this is exclusive
+            return neighbors.get(index);
+        }
+        return null;
+    }
+
+    static void tearDownWall(Room oldRoom, Room newRoom) {
+        // going up:
+        if (newRoom.row < oldRoom.row) {
+            newRoom.hasBottom = false;
+        }
+        // going down:
+        else if (newRoom.row > oldRoom.row) {
+            oldRoom.hasBottom = false;
+        }
+        // going left:
+        else if (newRoom.col < oldRoom.col) {
+            newRoom.hasRight = false;
+        }
+        // going right
+        else if (newRoom.col > oldRoom.col) {
+            oldRoom.hasRight = false;
+        }
+    }
+
+    static boolean createMaze(Room[][] rooms, Room room) {
+        room.wasVisited = true;
+        Room nextRoom = randomNeighbor(rooms, room.row, room.col);
+        if (nextRoom == null) {
+            return false;
+        }
+        tearDownWall(room, nextRoom);
+        //here's where we get to recursion: calling the method inside it self at this point.
+
+        while (createMaze(rooms, nextRoom));
+        return true;
+    }
+
+
     public static void main(String[] args) {
-	// write your code here
+        Room[][] rooms = createRooms();
+        createMaze(rooms, rooms[0][0]);
+        for (Room[] row : rooms) {
+            System.out.print(" _");
+        }
+        System.out.println();
+        for (Room[] row : rooms) {
+            System.out.print("|");
+            for (Room room : row) {
+                System.out.print(room.hasBottom ? "_" : " ");
+                System.out.print(room.hasRight ? "|" : " "); // inline conditional. if it has right, print pipe. if not, print space.
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/com/theironyard/Room.java
@@ -0,0 +1,19 @@
+package com.theironyard;
+
+/**
+ * Created by will on 7/13/16.
+ */
+public class Room {
+    int row;
+    int col;
+    boolean wasVisited = false;
+    boolean hasBottom = true;
+    boolean hasRight = true;
+
+    public Room(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+
+}
